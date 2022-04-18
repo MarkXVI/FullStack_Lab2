c4f267db533787da618d94e32e0fe6ffb735fa98
Mark Harvey <mark990521@gmail.com>
2022-04-18 23:41:46 +0200

    2341

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"; 
 import ReactDOM from "react-dom"; 
-import App from "./app";
+import App from "./__app";
  
 function Welcome(props) { 
   return <h1>Hello, {props.name}</h1>; 
@@ -11,10 +11,18 @@ const element = <Welcome name="Bob" />;
 ReactDOM.render(element, document.getElementById("index"));
 
 const rootElement = document.getElementById("root");
+
 ReactDOM.render(
   <React.StrictMode>
     <App/>
   </React.StrictMode>,
   rootElement
 );
-
+setInterval(function () {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>,
+    rootElement
+  );
+}, 60* 1000);
